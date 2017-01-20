475b23e4bff5c451272b9f38b7c1cb25da5982bb
oshkatov <oshkatov@twilio.com>
2017-01-20 23:04:34 +0200

    small fixes

--- default.py
+++ default.py
@@ -54,7 +54,7 @@ except:
     __addon__.setSetting('filter', '1')
     __addon__.setSetting('querry', '')
     # import datetime
-    __addon__.setSetting('year', 0) # str(datetime.date.today().year))
+    __addon__.setSetting('year', '0')  # str(datetime.date.today().year))
 
 __language__ = __addon__.getLocalizedString
 
